08dc28c505d142e352a00e6e13b43748a02aeafd
daman <damandhiman78880@gmail.com>
2022-09-13 15:24:54 +0530

    myFirstLibrary

--- PrintToast/src/main/java/com/daman/printtoast/PrintToast.kt
+++ PrintToast/src/main/java/com/daman/printtoast/PrintToast.kt
@@ -1,10 +1,10 @@
-package com.daman.customtesting
+package com.daman.printtoast
 
 import android.content.Context
 import android.widget.Toast
 
-object MyUtils {
-    fun printToast(context: Context,msg:String){
+object PrintToast {
+    fun print(context: Context,msg:String){
         Toast.makeText(context,msg,Toast.LENGTH_SHORT).show()
     }
 }
